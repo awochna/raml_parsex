f4ea080701087c3091653f7e352f6c01780e458b
Alex Wynter <awochna@email.arizona.edu>
2017-02-27 12:10:40 -0700

    Inital commit of generated files.

---
+++ lib/raml_parsex.ex
@@ -0,0 +1,18 @@
+defmodule RamlParsex do
+  @moduledoc """
+  Documentation for RamlParsex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RamlParsex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/raml_parsex_test.exs
@@ -0,0 +1,8 @@
+defmodule RamlParsexTest do
+  use ExUnit.Case
+  doctest RamlParsex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
